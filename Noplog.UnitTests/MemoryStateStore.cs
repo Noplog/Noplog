ad03c1c6c9b423b9171d3381e7ef5c317c7a8a93
Reuben Bond <reuben.bond@outlook.com>
2016-01-21 23:33:00 +1100

    Fully implement IPersistentState in MemoryStateStore

--- Noplog.UnitTests/MemoryStateStore.cs
+++ Noplog.UnitTests/MemoryStateStore.cs
@@ -4,6 +4,7 @@ namespace Noplog.UnitTests
 {
     public class MemoryStateStore : IPersistentState
     {
+        public string Identity { get; set; }
         public long CurrentTerm { get; set; }
         public string VotedFor { get; set; }
     }
